7210904caf3107cf5e14c8c16c49a1311c678f2f
Gustavo Bonassa <09845549985@edu.udesc.br>
2019-07-01 11:00:31 -0300

    first commit

--- config/database.js
+++ config/database.js
@@ -51,11 +51,11 @@ module.exports = {
   mysql: {
     client: 'mysql',
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      host: 'sql10.freemysqlhosting.net',
+      port: 3306,
+      user: 'sql10297257',
+      password: 'mRajgu36qV',
+      database: 'sql10297257'
     },
     debug: Env.get('DB_DEBUG', false)
   },
