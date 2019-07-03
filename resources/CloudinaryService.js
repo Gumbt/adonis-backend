e90d2c204267544e809716abe1a73872986b0c6e
Gustavo Bonassa <09845549985@edu.udesc.br>
2019-07-03 14:46:14 -0300

    websocket funfando

--- resources/CloudinaryService.js
+++ resources/CloudinaryService.js
@@ -2,9 +2,9 @@ const cloudinary = require('cloudinary').v2;
 const Env = use('Env');
 
 cloudinary.config({
-  cloud_name: Env.get('CLOUDINARY_CLOUD_NAME'),
-  api_key: Env.get('CLOUDINARY_API_KEY'),
-  api_secret: Env.get('CLOUDINARY_API_SECRET')
+  cloud_name: Env.get('CLOUDINARY_CLOUD_NAME', CLOUDINARY_CLOUD_NAME),
+  api_key: Env.get('CLOUDINARY_API_KEY', CLOUDINARY_API_KEY),
+  api_secret: Env.get('CLOUDINARY_API_SECRET', CLOUDINARY_API_SECRET)
 });
 
 module.exports = cloudinary;
