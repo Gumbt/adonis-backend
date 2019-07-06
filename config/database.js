204d4f0291cd569deebecaaacec2ddf34c4af532
Gumb <gustavo.bonassa1@gmail.com>
2019-07-05 22:47:18 -0300

    mudando db

--- config/database.js
+++ config/database.js
@@ -51,11 +51,11 @@ module.exports = {
     mysql: {
         client: 'mysql',
         connection: {
-            host: Env.get('DB_HOST', 'localhost'),
-            port: Env.get('DB_PORT', ''),
-            user: Env.get('DB_USER', 'root'),
-            password: Env.get('DB_PASSWORD', ''),
-            database: Env.get('DB_DATABASE', 'adonis')
+            host: Env.get('DB_HOST', process.env.DB_HOST),
+            port: Env.get('DB_PORT', process.env.DB_PORT),
+            user: Env.get('DB_USER', process.env.DB_USER),
+            password: Env.get('DB_PASSWORD', process.env.DB_PASSWORD),
+            database: Env.get('DB_DATABASE', process.env.DB_DATABASE)
         },
         debug: Env.get('DB_DEBUG', false)
     },
